6603c10638c8364657f0ee8bfe44ee1bfe78de8c
boorja <borja6168@gmail.com>
2024-12-07 17:11:19 +0100

    Unificacion de scripts Reto

--- Entrega_3/grafo.py
+++ Entrega_3/grafo.py
@@ -2,6 +2,12 @@ from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import RDF, XSD
 from pymongo import MongoClient
 import json
+import rdflib
+import re
+
+#==================================================#
+#                 GENERAR GRAFO                    #
+#==================================================#
 
 # Conexión a MongoDB
 uri = "mongodb+srv://Administrador:Grupo5@basededatos.wplin.mongodb.net/"
@@ -79,3 +85,44 @@ for vacunacion in vacunaciones_collection.find():
 
 # Guardar el grafo en formato Turtle
 g.serialize(destination="covid_data.ttl", format="turtle")
+
+
+#==================================================#
+#                 CONSULTAS CON GRAFO              #
+#==================================================#
+
+# Cargar el grafo RDF
+g = Graph()
+g.parse("covid_data.ttl", format="turtle")
+
+# Función para ejecutar una consulta SPARQL
+def execute_sparql_query(graph, query):
+    results = graph.query(query)
+    return results
+
+# Función para leer consultas y sus cabeceras desde un archivo
+def read_sparql_queries(filepath):
+    with open(filepath, "r") as f:
+        content = f.read()
+    queries = content.split("\n\n\n")
+    result = []
+    for q in queries:
+        if q.strip():
+            header = re.search(r"^#\s*(.*)", q.strip(), re.MULTILINE).group(1)
+            query_body = q.strip()
+            result.append((header, query_body))
+    return result
+
+# Leer consultas y cabeceras desde el archivo
+queries_with_headers = read_sparql_queries("consultas.sparql")
+
+# Ejecutar y mostrar los resultados de cada consulta
+for header, query in queries_with_headers:
+    print(f"\n{header}")  # Imprimir la cabecera
+    results = execute_sparql_query(g, query)
+
+    if not results:
+        print("Respuesta Vacía")
+    else:
+        for row in results:
+            print(", ".join([str(item.value) if isinstance(item, rdflib.term.Literal) else str(item) for item in row]))
